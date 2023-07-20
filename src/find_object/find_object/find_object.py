3e2ed21c379ed9258cdb5b66d38dee45de8928f1
jfernandez37 <jothomas0615@gmail.com>
2023-07-20 09:17:55 -0400

    Removes small contours

--- src/find_object/find_object/find_object.py
+++ src/find_object/find_object/find_object.py
@@ -23,17 +23,22 @@ class FindObject(Node):
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
-
+    
+    def find_correct_contour(self, contours):
+        areas = [cv2.contourArea(cnt) for cnt in contours]
+        return areas.index(max(areas))
+    
     def listener_callback(self, msg):
         thresh_value = self.get_parameter('thresh_value').get_parameter_value().integer_value
-        self.cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgra8')[110:400, 170:600]#y,x
+        self.cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgra8')#[110:400, 170:600]#y,x
         gray_img = cv2.cvtColor(self.cv_image,cv2.COLOR_BGR2GRAY)
         gray_img = gray_img
         blurred_img = cv2.blur(gray_img, (5,5))
         _,self.thresh_image = cv2.threshold(blurred_img,thresh_value,255,0)
         contours, _ = cv2.findContours(self.thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        print(len(contours))
         cv2.drawContours(self.cv_image, contours, -1, (0,255,0), 3)
-        cnt = contours[0]
+        cnt = contours[self.find_correct_contour(contours)]
         M = cv2.moments(cnt)
         # print( M )
         try:
@@ -44,4 +49,6 @@ class FindObject(Node):
             img_w_center = cv2.circle(self.cv_image, (cx,cy), radius=5, color=(0,0,255), thickness=-1)
         except:
             print("Error: Contour does not form a single shape")
+        
+    
         
