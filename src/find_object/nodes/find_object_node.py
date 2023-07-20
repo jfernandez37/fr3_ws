320d11bbfa7f0d97c09127c5a0f205371ed609f6
Joe Fernandez <52220317+jfernandez37@users.noreply.github.com>
2023-07-20 09:03:44 -0400

    Add files via upload

---
+++ src/find_object/nodes/find_object_node.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import cv2
+import rclpy
+
+from find_object.find_object import FindObject
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    find_object = FindObject()
+
+    while find_object.cv_image is None:
+        rclpy.spin_once(find_object)
+    cv2.imshow("Image", find_object.cv_image)
+    cv2.imshow("Thresh", find_object.thresh_image)
+    cv2.waitKey(0)
+
+    find_object.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
