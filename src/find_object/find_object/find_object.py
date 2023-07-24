c453b8547d79c3b7ac2d3672c690ce0f661a7715
Boogsy679 <jothomas0615@gmail.com>
2023-07-24 09:41:16 -0400

    updated listener_callback and added find_measurements

--- src/find_object/find_object/find_object.py
+++ src/find_object/find_object/find_object.py
@@ -26,7 +26,6 @@ class FindObject(Node):
     def closest_to_circle(self, contours):
         circle_areas = []
         areas = [cv2.contourArea(cnt) for cnt in contours]
-        print(areas)
         for cnt in contours:
             (x,y),radius = cv2.minEnclosingCircle(cnt)
             circle_areas.append(__import__("math").pi * radius**2)
@@ -45,9 +44,8 @@ class FindObject(Node):
         return filtered_contours
                 
     
-    def listener_callback(self, msg):
-        thresh_value = self.get_parameter('thresh_value').get_parameter_value().integer_value
-        self.cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgra8')#[110:400, 170:600]#y,x
+    def listener_callback(self, path, thresh_value):
+        self.cv_image = cv2.imread(path, cv2.IMREAD_COLOR)
         gray_img = cv2.cvtColor(self.cv_image,cv2.COLOR_BGR2GRAY)
         blurred_img = cv2.GaussianBlur(gray_img,(7,7),0)
         self.blurred_image = blurred_img
@@ -55,24 +53,29 @@ class FindObject(Node):
         contours, _ = cv2.findContours(self.thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         before_remove = len(contours)
         contours = self.remove_bad_contours(contours)
-        print(before_remove-len(contours), " contours were removed")
-        # for cnt in contours:
-        #     (x,y),radius = cv2.minEnclosingCircle(cnt)
-        #     center = (int(x),int(y))
-        #     radius = int(radius)
-        #     cv2.circle(self.cv_image,center,radius,(0,255,0),2)
+        print(before_remove-len(contours), " contours were removed")            
         cv2.drawContours(self.cv_image, contours, -1, (0,255,0), 3)
-        # M = cv2.moments(contours[self.find_correct_contour(contours)]) # Finds contour with largest area
         M = cv2.moments(contours[self.closest_to_circle(contours)]) #Finds the contour that is closest to a circle
+        (x,y),self.radius = cv2.minEnclosingCircle(contours[self.closest_to_circle(contours)])
+        (x,y),radius = cv2.minEnclosingCircle(contours[self.closest_to_circle(contours)])
+        center = (int(x),int(y))
+        radius = int(radius)
+        cv2.circle(self.cv_image,center,radius,(255,0,0),2)
         # print( M )
         try:
-            print("m10:",M['m10'],"m01:",M['m01'], "m00:",M['m00'])
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            # print("x:",cx," y:",cy)
-            img_w_center = cv2.circle(self.cv_image, (cx,cy), radius=10, color=(0,0,255), thickness=-1)
+            (h, w) = self.cv_image.shape[:2]
+            self.cx = w//2
+            self.cy = h//2
+            cv2.circle(self.cv_image, (w//2, h//2), 7, (255, 255, 255), -1)
+            self.gx = int(M['m10']/M['m00'])
+            self.gy = int(M['m01']/M['m00'])
+            cv2.circle(self.cv_image, (self.gx,self.gy), 10, color=(0,0,255), thickness=-1)
+            cv2.circle(self.cv_image, (self.gx+int(radius),self.gy), 10, color=(255,0,0), thickness=-1)
         except:
             print("Error: Contour does not form a single shape")
+        
+    def find_measurements(self):
+        return (self.gx-self.cx) * self.gear_radius_size/self.radius, (self.gy-self.cy)*self.gear_radius_size/self.radius
         
     
         
