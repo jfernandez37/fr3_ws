70c70cae03804bd6e50fa9452f5891d440af59c5
Boogsy679 <jothomas0615@gmail.com>
2023-07-28 13:32:47 -0400

    Documentation and removed old code

--- src/gear_place/gear_place/find_object.py
+++ src/gear_place/gear_place/find_object.py
@@ -1,21 +1,17 @@
 import cv2
 import rclpy
 from rclpy.node import Node
-# from ament_index_python.packages import get_package_share_directory, PackageNotFoundError
 
 import numpy as np
 
-from sensor_msgs.msg import PointCloud2
-from sensor_msgs_py.point_cloud2 import read_points_numpy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image # msg for recieving the image
 
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge # for converting the sensor_msgs.msg.image to opencv image
 
 class FindObject(Node):
 
     def __init__(self):
         super().__init__('find_object')
-        self.thresh_image = None
         self.bridge = CvBridge()
         self.cv_image = None
         self.declare_parameter('thresh_value', 30)
@@ -27,15 +23,21 @@ class FindObject(Node):
         self.subscription  # prevent unused variable warning
     
     def closest_to_circle(self, contours):
+        '''
+        Returns the contour which is closest to a circle
+        '''
         circle_areas = []
         areas = [cv2.contourArea(cnt) for cnt in contours]
         for cnt in contours:
-            (x,y),radius = cv2.minEnclosingCircle(cnt)
+            (_,_),radius = cv2.minEnclosingCircle(cnt)
             circle_areas.append(__import__("math").pi * radius**2)
         diffs = [areas[i]/circle_areas[i] for i in range(len(areas))]
         return diffs.index(max(diffs))
     
     def remove_bad_contours(self, contours : tuple):
+        '''
+        Removes contours which are too small and ones with too few sides to be the gear
+        '''
         minimum_contour_area = 500
         new_contours = [cnt for cnt in contours if not cv2.isContourConvex(cnt)]
         filtered_contours=[]
@@ -48,8 +50,12 @@ class FindObject(Node):
                 
     
     def listener_callback(self, msg):
+        '''
+        Gets the image from the contour, blurs it, applies a threshold, finds the contours.
+        Then, the functions above are used to find the gear out of all the contours that are found.
+        It then finds the center of the gear contour.
+        '''
         thresh_value = self.get_parameter('thresh_value').get_parameter_value().integer_value
-        # self.get_logger().info(f"Size of the image {self.cv_image.shape}")
         cv_image = self.bridge.imgmsg_to_cv2(msg, "32FC1")
         cv_image_array = np.array(cv_image, dtype = np.dtype('u1'))
         self.cv_image = cv_image_array
@@ -65,7 +71,6 @@ class FindObject(Node):
         (x,y),self.radius = cv2.minEnclosingCircle(contours[self.closest_to_circle(contours)])
         center = (int(x),int(y))
         cv2.circle(self.cv_image,center,int(self.radius),(255,255,255),2)
-        # print( M )
         try:
             (h, w) = self.cv_image.shape[:2]
             self.cx = w//2
@@ -74,13 +79,15 @@ class FindObject(Node):
             self.gx = int(M['m10']/M['m00'])
             self.gy = int(M['m01']/M['m00'])
             cv2.circle(self.cv_image, (self.gx,self.gy), 10, color=(255,255,255), thickness=-1)
-            # cv2.circle(self.cv_image, (self.gx+int(radius),self.gy), 10, color=(255,255,255), thickness=-1)
         except:
             print("Error: Contour does not form a single shape")
         self.get_logger().info(f"X coordinate for gear: {self.gx}, y coordinate for gear {self.gy}")
         
 
     def ret_cent_gear(self):
+        '''
+        Returns the x and y coordinates of the pixel at the center of the gear
+        '''
         return self.gx, self.gy
         
     
