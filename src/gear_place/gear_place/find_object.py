55df82b6579b6b71f6caf5c2f5dc1dda2ec6de58
jfernandez37 <jothomas0615@gmail.com>
2023-08-01 14:19:00 -0400

    undid changes

--- src/gear_place/gear_place/find_object.py
+++ src/gear_place/gear_place/find_object.py
@@ -60,14 +60,26 @@ class FindObject(Node):
         '''
         thresh_value = self.get_parameter('thresh_value').get_parameter_value().integer_value
         cv_image = self.bridge.imgmsg_to_cv2(msg, "32FC1")
-        self.cv_image = np.array(cv_image, dtype = np.dtype('u1'))
+        cv_image_array = np.array(cv_image, dtype = np.dtype('u1'))
+        self.cv_image = cv_image_array
         alpha = 1 # Contrast control (1.0-3.0)
         beta = -50 # Brightness control (0-100)
         self.cv_image = cv2.convertScaleAbs(self.cv_image, alpha=alpha, beta=beta)
-        for i in range(len(self.cv_image)):
-            for j in range(len(self.cv_image[i])):
-                self.cv_image[i][j] = 255 if self.cv_image[i][j]<50 else 0
-        contours, _ = cv2.findContours(self.cv_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for i in range(len(cv_image)):
+            for j in range(len(cv_image[i])):
+                self.cv_image[i][j] = 0 if self.cv_image[i][j]<39 else 255
+        blurred_img = cv2.GaussianBlur(self.cv_image,(7,7),0)
+        _,self.thresh_image = cv2.threshold(blurred_img,thresh_value,255,cv2.THRESH_BINARY_INV)
+        d={}
+        for i in self.thresh_image:
+            for j in i:
+                try: 
+                    d[j]+=1
+                except:
+                    d[j]=1
+        print(d)
+        # self.thresh_image = cv2.adaptiveThreshold(,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+        contours, _ = cv2.findContours(self.thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         print("Contours found:",len(contours))
         before_remove = len(contours)
         contours = self.remove_bad_contours(contours)
