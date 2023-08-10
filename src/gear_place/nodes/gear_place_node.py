838d9137998b64c96a371566cf610ef3e7cea05a
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-10 09:50:06 -0400

    Added sleep and changed while loop conditions

--- src/gear_place/nodes/gear_place_node.py
+++ src/gear_place/nodes/gear_place_node.py
@@ -4,7 +4,7 @@ import rclpy
 from gear_place.gear_place_classes import GearPlace, Error
 from gear_place.find_object import FindObject
 from gear_place.object_depth import ObjectDepth
-
+from time import sleep
 
 def main(args=None):
     gear_center_target = [0 for i in range(3)]
@@ -14,11 +14,11 @@ def main(args=None):
         supervisor = GearPlace("gear_place")
         supervisor.wait(2.0)
         supervisor._call_move_to_named_pose_service("home")
-        __import__("time").sleep(3)
+        sleep(3)
         supervisor._call_move_cartesian_service(-0.20, 0.0, 0.0, 0.15, 0.2)
-        __import__("time").sleep(1)
+        sleep(1)
         
-        while gear_center_target.count(0) == 3:
+        while gear_center_target.count(0) == 3 and None not in gear_center_target:
             find_object = FindObject()
             rclpy.spin_once(find_object)
             object_depth = ObjectDepth(find_object.ret_cent_gear())
@@ -30,6 +30,8 @@ def main(args=None):
             gear_center_target[1] = object_depth.dist_y
             gear_center_target[2] = object_depth.dist_z
             print(gear_center_target)
+            sleep(1)
+        sleep(3)
         supervisor._call_move_cartesian_service(object_depth.dist_x,object_depth.dist_y,0.0,0.15,0.2)
         #     print(gear_center_target)
         # supervisor._call_pick_up_gear_service(
