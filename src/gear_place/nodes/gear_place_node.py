6aa980bd7cd35608485d7c2d6018a3bcd4b4b592
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-10 10:04:22 -0400

    maybe

--- src/gear_place/nodes/gear_place_node.py
+++ src/gear_place/nodes/gear_place_node.py
@@ -33,7 +33,7 @@ def main(args=None):
         sleep(3)
         print(gear_center_target)
         cv2.imshow("Depth image", find_object.cv_image)
-        supervisor._call_move_cartesian_service(-1*object_depth.dist_y,object_depth.dist_x,0.0,0.15,0.2)
+        supervisor._call_move_cartesian_service(object_depth.dist_y,object_depth.dist_x,0.0,0.15,0.2)
         #     print(gear_center_target)
         # supervisor._call_pick_up_gear_service(
         #     object_depth.dist_x, object_depth.dist_y, object_depth.dist_z, 0.01
