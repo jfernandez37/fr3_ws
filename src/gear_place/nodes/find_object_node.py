915b41f6975cc8003332fecd169a164f59b5fede
jfernandez37 <jothomas0615@gmail.com>
2023-08-10 16:02:27 -0400

    Removed old code

--- src/gear_place/nodes/find_object_node.py
+++ src/gear_place/nodes/find_object_node.py
@@ -11,12 +11,7 @@ def main(args=None):
     gear_center_values = [0 for i in range(3)]
     while gear_center_values.count(0) == 3:
         find_object = FindObject()
-        # rclpy.spin(find_object)
         rclpy.spin_once(find_object)
-
-        # cv2.imshow("Originial image", find_object.original_image)
-
-        # __import__("time").sleep(1)
         object_depth = ObjectDepth(find_object.ret_cent_gear())
         rclpy.spin_once(object_depth)
 
@@ -25,7 +20,6 @@ def main(args=None):
         gear_center_values[0] = object_depth.dist_x
         gear_center_values[1] = object_depth.dist_y
         gear_center_values[2] = object_depth.dist_z
-        print(gear_center_values)
     cv2.imshow("Threshold image", find_object.thresh_image)
     cv2.imshow("Depth image", find_object.cv_image)
     cv2.waitKey(0)
