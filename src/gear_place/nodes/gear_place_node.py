81be34f71e4d2fe8222a0082f26a224fe2e43e49
jfernandez37 <jothomas0615@gmail.com>
2023-08-17 14:03:06 -0400

    Cleaned up to make more readable and removed old stuff

--- src/gear_place/nodes/gear_place_node.py
+++ src/gear_place/nodes/gear_place_node.py
@@ -2,32 +2,22 @@
 
 import rclpy
 from gear_place.gear_place_classes import GearPlace, Error, ConveyorClass
-from gear_place.find_object import FindObject
-from gear_place.object_depth import ObjectDepth
+
 from time import sleep
 
 
 def main(args=None):
-    gear_center_target = [0 for i in range(3)]
     rclpy.init(args=args)
 
     try:
         supervisor = GearPlace("gear_place")
         conveyor_supervisor = ConveyorClass("aprs_ros_conveyor")
         supervisor.wait(0.75)
-        supervisor._call_move_to_named_pose_service(
-            "home"
-        )  # starts in the home position
+        supervisor._call_move_to_named_pose_service("home")  # starts in the home position
         sleep(3)
-        supervisor._call_move_cartesian_service(
-            -0.27, 0.0, 0.0, 0.15, 0.2
-        )  # Moves to the center of the cart
+        supervisor._call_move_cartesian_service(-0.27, 0.0, 0.0, 0.15, 0.2)  # Moves to the center of the cart
         sleep(1)
-
-        
-        supervisor._call_pick_up_gear_service(
-            0.0095,
-        )  # Moves to above the gear, opens the gripper to the maximum, then down to the gear, grabs the gear, then picks it up
+        supervisor._call_pick_up_gear_service(0.0095)  # Moves to above the gear, opens the gripper to the maximum, then down to the gear, grabs the gear, then picks it up
         sleep(1.5)
         supervisor._call_put_gear_down_service()  # moves down, releases the gear, and moves back up
         """
