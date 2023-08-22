c705d5f0ee246ac861718cd557f79fe46644a759
jfernandez37 <jothomas0615@gmail.com>
2023-08-22 16:34:54 -0400

    Formatting

--- src/gear_place/nodes/moving_gear_node.py
+++ src/gear_place/nodes/moving_gear_node.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import rclpy
 from gear_place.moving_gear import MovingGear
-    
+
+
 def main(args=None):
     rclpy.init(args=args)
     moving_gear = MovingGear()
     for i in range(12):
-        print(f"After {round(moving_gear.times[1]+i/4,2)} seconds from starting time, the gear is ",str(moving_gear.point_from_time(moving_gear.times[1]+i/4)),"away from the center of the camera")
+        print(
+            f"After {round(moving_gear.times[1]+i/4,2)} seconds from starting time, the gear is ",
+            str(moving_gear.point_from_time(moving_gear.times[1] + i / 4)),
+            "away from the center of the camera"
+        )
     rclpy.shutdown()
 
 
