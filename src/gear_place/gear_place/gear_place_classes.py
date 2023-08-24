4574e0ec8cddfdbf4babae07239d65922a135546
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-24 11:37:00 -0400

    increased time for delays

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -249,7 +249,7 @@ class GearPlace(Node):
         velocity = 0.15
         acceleration = 0.2
         pick_up_constant = (
-            velocity / acceleration + abs(z_movement) / velocity + 0.05
+            velocity / acceleration + abs(z_movement) / velocity + 0.1
         )  # time that it takes for the fr3 to open gripper,move down, and grasp the gear
         slope, intercept = moving_gear.distance_formula()
 
