861b82d4571b84405c34b576329a0417ac4bc373
jfernandez37 <jothomas0615@gmail.com>
2023-08-29 13:42:57 -0400

    Ensures gripper is open

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -359,6 +359,7 @@ class GearPlace(Node):
         distances_from_home = self.remove_identical_points(distances_from_home)
         
         for gear_point in distances_from_home:
+            self._call_open_gripper_service()
             self._call_move_to_named_pose_service("home")
             self._call_pick_up_gear_coord_service(gear_point[0],gear_point[1], 0.0095)
             self._call_put_gear_down_service()
