1a2f97070cf7c635a766092115e7fda4cbb07f8a
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-29 14:50:31 -0400

    Testing

--- src/gear_place/gear_place/multiple_gears.py
+++ src/gear_place/gear_place/multiple_gears.py
@@ -19,7 +19,7 @@ class MultipleGears(Node):
         self.thresh_image = None
         self.declare_parameter("thresh_value", 50)
         self.subscription = self.create_subscription(
-            Image, "/camera/depth/image_rect_raw", self.listener_callback, 10
+            Image, "/camera/depth/image_rect_raw", self.listener_callback, 50
         )
         self.subscription  # prevent unused variable warning
 
