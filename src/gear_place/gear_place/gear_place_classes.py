a21b7c71bef0068f537f8484b5faaf3916d838e5
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-29 14:36:58 -0400

    Still not working

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -334,26 +334,25 @@ class GearPlace(Node):
         self.get_logger().info(f"Picking up gear")
         gear_center_target = [[0 for i in range(3)]]
         for ind in range(len(robot_moves)):
-            for i in range(3):
-                while (
-                    [0, 0, 0] in gear_center_target
-                    or sum([cent.count(None) for cent in gear_center_target]) > 0
-                ) and len(gear_center_target) > 0:
-                    gear_center_target = []
+            while (
+                [0, 0, 0] in gear_center_target
+                or sum([cent.count(None) for cent in gear_center_target]) > 0
+            ) and len(gear_center_target) > 0:
+                gear_center_target = []
+                multiple_gears = MultipleGears()
+                rclpy.spin_once(multiple_gears)
+                while sum([cent.count(None) for cent in multiple_gears.g_centers]) != 0:
+                    multiple_gears.destroy_node()
                     multiple_gears = MultipleGears()
                     rclpy.spin_once(multiple_gears)
-                    while sum([cent.count(None) for cent in multiple_gears.g_centers]) != 0:
-                        multiple_gears.destroy_node()
-                        multiple_gears = MultipleGears()
-                        rclpy.spin_once(multiple_gears)
-                    for g_center in multiple_gears.g_centers:
-                        object_depth = ObjectDepth(g_center)
-                        rclpy.spin_once(object_depth)  # Gets the distance from the camera
-                        object_depth.destroy_node()  # Destroys the node to avoid errors on next loop
-                        gear_center_target.append(
-                            [object_depth.dist_x, object_depth.dist_y, object_depth.dist_z]
-                        )
-                    multiple_gears.destroy_node()
+                for g_center in multiple_gears.g_centers:
+                    object_depth = ObjectDepth(g_center)
+                    rclpy.spin_once(object_depth)  # Gets the distance from the camera
+                    object_depth.destroy_node()  # Destroys the node to avoid errors on next loop
+                    gear_center_target.append(
+                        [object_depth.dist_x, object_depth.dist_y, object_depth.dist_z]
+                    )
+                multiple_gears.destroy_node()
             for arr in gear_center_target:
                 distances_from_home.append(
                     (-1 * arr[1] + x_movements[:ind], -1 * arr[0] + y_movements[:ind])
