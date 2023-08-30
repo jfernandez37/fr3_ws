ad7350aa85ddff72ebbb2a5542245ffa3b5ad4e5
jfernandez37 <jothomas0615@gmail.com>
2023-08-30 11:22:42 -0400

    Getting to work with the small gears

--- src/gear_place/gear_place/multiple_gears.py
+++ src/gear_place/gear_place/multiple_gears.py
@@ -40,7 +40,7 @@ class MultipleGears(Node):
         """
         Removes contours which are too small and ones with too few sides to be the gear
         """
-        minimum_contour_area = 4000
+        minimum_contour_area = 3000
         maximum_contour_area = 15000
         new_contours = [cnt for cnt in contours if not cv2.isContourConvex(cnt)]
         filtered_contours = []
