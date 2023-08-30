a4c0e315dea9b11a85e892673f99d120e6bc2194
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-30 16:26:34 -0400

    X offset

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -516,7 +516,7 @@ class GearPlace(Node):
         """
         Calls the pick_up_gear callback
         """
-        self.x_offset = 0.041  # offset from the camera to the gripper
+        self.x_offset = 0.03975  # offset from the camera to the gripper
         self.y_offset = 0.03  # offset from the camera to the gripper
         z_movement = max(
             -0.247, z+0.048
