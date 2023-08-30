90353bb85885b1502d3046cd334d97c48e2404dc
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-30 10:53:18 -0400

    Weird issue

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -485,7 +485,7 @@ class GearPlace(Node):
         else:
             request.x = x
             request.y = y    
-        request.z = z_movement+.0005
+        request.z = z_movement+.0015
         request.object_width = object_width
 
         future = self.create_client(PickUpGear, "pick_up_gear").call_async(request)
