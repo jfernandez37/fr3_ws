ecf8dfb4f597f67148d41abaa675ba46836b5373
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-30 10:31:00 -0400

    Offset does not work

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -411,10 +411,7 @@ class GearPlace(Node):
         next_move = [0,0]
         self._call_move_to_named_pose_service("home")
         for gear_point in distances_from_home: # loops through the movements to the gears
-            if next_move != [0,0]:
-                next_move = [gear_point[i]-next_move[i]-0.03 for i in range(2)] # finds the next movement to the next gear
-            else:
-                next_move = gear_point
+            next_move = [gear_point[i]-next_move[i]*1.25 for i in range(2)] # finds the next movement to the next gear
             self.get_logger().info("Next_move:"+str(next_move))
             # self._call_move_to_named_pose_service("home")
             self._call_open_gripper_service() # opens the gripper
