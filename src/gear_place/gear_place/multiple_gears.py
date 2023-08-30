886797e02f3a764c94cf01f457e94af077fd6cca
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-30 15:30:08 -0400

    Different thresh ranges

--- src/gear_place/gear_place/multiple_gears.py
+++ src/gear_place/gear_place/multiple_gears.py
@@ -62,7 +62,8 @@ class MultipleGears(Node):
         It then finds the center of the gear contour.
         """
         self.ran = True
-        min_thresh, max_thresh = 25, 75  # works on fr3
+        # min_thresh, max_thresh = 25, 75  # works on fr3
+        min_thresh, max_thresh = 0,255
         # min_thresh, max_thresh = 150, 225
         thresh_value = (
             self.get_parameter("thresh_value").get_parameter_value().integer_value
