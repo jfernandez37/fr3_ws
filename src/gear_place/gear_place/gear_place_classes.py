bcf95abca77d772e88669295d4e3c4f790af9cf6
Joseph Fernandez <jothomas0615@gmail.com>
2023-08-30 10:15:39 -0400

    Testing

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -411,11 +411,12 @@ class GearPlace(Node):
         next_move = [0,0]
         self._call_move_to_named_pose_service("home")
         for gear_point in distances_from_home: # loops through the movements to the gears
-            next_move = [gear_point[i]-next_move[i] for i in range(2)] # finds the next movement to the next gear
+            # next_move = [gear_point[i]-next_move[i] for i in range(2)] # finds the next movement to the next gear
             self.get_logger().info("Next_move:"+str(next_move))
+            self._call_move_to_named_pose_service("home")
             self._call_open_gripper_service() # opens the gripper
             self._call_pick_up_gear_coord_service(
-                next_move[0], next_move[1], object_width
+                gear_point[0], gear_point[1], object_width
             ) # picks up the gear
             self._call_put_gear_down_service() # puts the gear down
 
