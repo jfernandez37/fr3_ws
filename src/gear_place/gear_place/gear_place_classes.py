9d78c024edf369620b8bc97263e6743c754345ca
jfernandez37 <jothomas0615@gmail.com>
2023-09-05 16:45:51 -0400

    Breaks infinite loop for the depth vals

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -513,7 +513,9 @@ class GearPlace(Node):
         depth_vals = []
         x_center = 100
         y_center = 100
+        c=0
         while len(depth_vals) < 7:
+            c+=1
             self.get_logger().info(
             "Depth Values:" + ", ".join([str(val) for val in depth_vals])
             )
@@ -529,6 +531,8 @@ class GearPlace(Node):
                     depth_vals.append(coord[2])
             x_center += 3
             y_center += 3
+            if c>=10 and len(depth_vals)<7:
+                depth_vals = [-100 for _ in range(10)]
 
         self.get_logger().info(
             "Depth Values:" + ", ".join([str(val) for val in depth_vals])
