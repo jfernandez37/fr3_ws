4b488a1fa29bf6abbd7eeeb86ae684805f1f8124
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-12 13:35:35 -0400

    Testing

--- src/gear_place/src/robot_commander.cpp
+++ src/gear_place/src/robot_commander.cpp
@@ -251,7 +251,7 @@ void RobotCommander::put_down_force_cb_(const std::shared_ptr<gear_place_interfa
   */
   try{
     while(!put_down_force(request->force)){
-      move_robot_cartesian(0.0,0.0,0.001,default_velocity_/4,default_acceleration_);
+      move_robot_cartesian(0.0,0.0,0.01,default_velocity_/4,0.5);
     }
     sleep(5.0);
     open_gripper();
