156236266b3ebae96223cd4264be5f87c4145dbe
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-12 11:21:24 -0400

    Print Z values

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -284,6 +284,7 @@ class GearPlace(Node):
       Takes in a list of points and returns the average of them
       """
       num_points = len(arr)
+      print([arr[i][2] for i in range(num_points)])
       return (
           sum([arr[i][0] for i in range(num_points)])/num_points,
           sum([arr[i][1] for i in range(num_points)])/num_points,
