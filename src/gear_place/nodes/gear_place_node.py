8036f2c89499049b4edbfe565e76388e77fc0696
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-14 13:58:13 -0400

    Fix issues

--- src/gear_place/nodes/gear_place_node.py
+++ src/gear_place/nodes/gear_place_node.py
@@ -8,8 +8,7 @@ def main(args=None):
     rclpy.init(args=args)
     gear_width = 0.0095
     try:
-        supervisor = GearPlace("gear_place")
-        supervisor = GearPlace("gear_place")
+        supervisor = GearPlace()
         conveyor_supervisor = ConveyorClass("aprs_ros_conveyor")
         supervisor.wait(3)
         supervisor._call_open_gripper_service()
