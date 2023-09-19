59f490b4bce0d5c7a3e8be280c9657ba02ee97e1
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-19 16:56:25 -0400

    before and after

--- src/gear_place/src/robot_commander.cpp
+++ src/gear_place/src/robot_commander.cpp
@@ -260,6 +260,7 @@ void RobotCommander::put_down_force_cb_(const std::shared_ptr<gear_place_interfa
     while(!(put_down_force(request->force))){
       sleep(1.0);
       move_robot_cartesian(0.0,0.0,-0.01,default_velocity_,0.5);
+      sleep(1.0);
     }
     // successful_put_down = put_down_force(request->force);
     // if(!successful_put_down){
