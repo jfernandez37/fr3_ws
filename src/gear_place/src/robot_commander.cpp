e2b89db89adcf765b2718eb2aa7c1ed26e9e5929
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-20 10:28:29 -0400

    Testing

--- src/gear_place/src/robot_commander.cpp
+++ src/gear_place/src/robot_commander.cpp
@@ -312,8 +312,7 @@ bool RobotCommander::put_down_force(double force)
   catch (const franka::Exception &e)
   {
     read_state_.unlock();
-    std::string ex = e.what();
-    RCLCPP_ERROR(get_logger(), "Could not run force motion generator. Error: "+ex);
+    RCLCPP_ERROR(get_logger(), "Could not run force motion generator. Error: "+e.what());
     return false;
   }  
   return force_motion_generator->get_result();
