0a886275efb010cb1f59f2c39e79c95835bec4fc
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-21 10:46:38 -0400

    Move up before scanning

--- src/gear_place/nodes/gear_place_node.py
+++ src/gear_place/nodes/gear_place_node.py
@@ -14,6 +14,7 @@ def main(args=None):
         supervisor._call_open_gripper_service()
         supervisor._call_move_to_named_pose_service("home")  # starts in the home position
         supervisor._call_move_to_named_pose_service("above_conveyor")
+        supervisor._call_move_cartesian_service(0.0,0.0,0.07, 0.15, 0.2)
         # # supervisor._call_move_cartesian_service(-0.3, 0.0, 0.0, 0.15, 0.2)  # Moves to the center of the cart
         # supervisor._call_open_gripper_service() # opens the gripper so that time delay is easier to calculate on moving gear pick up
         # supervisor._call_pick_up_multiple_gears(gear_width)
