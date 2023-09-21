8e728b37b79d506af93ac90aa4decf6e1ea9a5ac
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-21 10:09:21 -0400

    Getting coordinates for above conveyor belt

--- src/gear_place/nodes/gear_place_node.py
+++ src/gear_place/nodes/gear_place_node.py
@@ -10,9 +10,9 @@ def main(args=None):
     try:
         supervisor = GearPlace()
         conveyor_supervisor = ConveyorClass("aprs_ros_conveyor")
-        supervisor.wait(3)
+        supervisor.wait(300)
         supervisor._call_open_gripper_service()
-        supervisor._call_move_to_named_pose_service("home")  # starts in the home position
+        # supervisor._call_move_to_named_pose_service("home")  # starts in the home position
         # # supervisor._call_move_cartesian_service(-0.3, 0.0, 0.0, 0.15, 0.2)  # Moves to the center of the cart
         # supervisor._call_open_gripper_service() # opens the gripper so that time delay is easier to calculate on moving gear pick up
         # supervisor._call_pick_up_multiple_gears(gear_width)
