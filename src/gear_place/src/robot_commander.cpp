7401ed73287b69c8a14e7ff9ec7bdf4545511ac6
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-21 14:52:14 -0400

    Moves down more

--- src/gear_place/src/robot_commander.cpp
+++ src/gear_place/src/robot_commander.cpp
@@ -266,6 +266,7 @@ void RobotCommander::put_down_force_cb_(const std::shared_ptr<gear_place_interfa
     successful_put_down = put_down_force(request->force);
     sleep(1.0);
     while(!successful_put_down){
+      move_robot_cartesian(0.0,0.0,-0.01,default_velocity_,0.5);
       c+=1;
       successful_put_down = put_down_force(request->force);
       if(c%11==0){
