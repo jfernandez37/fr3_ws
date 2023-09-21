37a0a29bd89b83c5f18e1b1507b693b5e6a4bc9d
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-21 13:58:57 -0400

    Added movement if the arm is not rotated

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -244,7 +244,7 @@ class GearPlace(Node):
           self.get_logger().error(f"Unable to put gear down")
           raise Error("Unable to put gear down")
 
-  def _call_move_above_gear(self):
+  def _call_move_above_gear(self, rotated : bool):
       """
       Moves the robot above the gear
       """
@@ -279,12 +279,15 @@ class GearPlace(Node):
       
       x_value, y_value = moving_gear.point_from_time(intersection_time)
       
-      self._call_move_cartesian_service((x_value * -1 + Y_OFFSET)*-1,y_value * -1 + X_OFFSET,0.0,velocity, acceleration)
 
       final_x, final_y = moving_gear.point_from_time(intersection_time*3)
       gear_speed = (distance_between_two_points(moving_gear.x_vals, moving_gear.y_vals)) / (moving_gear.times[1]-moving_gear.times[0])
-
-      self._call_move_cartesian_service((final_x * -1 + Y_OFFSET)*-1, final_y * -1 + X_OFFSET, 0.0, gear_speed, 0.3)
+      if rotated:
+        self._call_move_cartesian_service((x_value * -1 + Y_OFFSET)*-1,y_value * -1 + X_OFFSET,0.0,velocity, acceleration)
+        self._call_move_cartesian_service((final_x * -1 + Y_OFFSET)*-1, final_y * -1 + X_OFFSET, 0.0, gear_speed, 0.3)
+      else:
+        self._call_move_cartesian_service(y_value * -1 + X_OFFSET,x_value * -1 + Y_OFFSET,0.0,velocity, acceleration)
+        self._call_move_cartesian_service(final_y * -1 + X_OFFSET,final_x * -1 + Y_OFFSET, 0.0, gear_speed, 0.3)
 
   
   def _call_pick_up_moving_gear_service(self, object_width : float, rotated : bool):
