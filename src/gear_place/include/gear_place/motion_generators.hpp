5e1448699c2d4d63eb7d461a81473d5e08ebc9e8
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-26 10:24:14 -0400

    May be a solution

--- src/gear_place/include/gear_place/motion_generators.hpp
+++ src/gear_place/include/gear_place/motion_generators.hpp
@@ -353,8 +353,9 @@ private:
 ForceMotionGenerator::ForceMotionGenerator(double force, franka::Model &model, franka::RobotState &state, bool* val)
     : force_(force), model_(model), state_(state), initial_tau_ext_(7), tau_error_integral_(7)
 {
-  std::cout << "IN concstructor: "<<*val <<std::endl;
   on_surface_ = val;
+  *on_surface_ = false;
+  std::cout << "IN concstructor: "<<*on_surface_ <<std::endl;
   gravity_array = model_.gravity(state_);
 
   Eigen::Map<Eigen::Matrix<double, 7, 1>> initial_tau_measured(state_.tau_J.data());
