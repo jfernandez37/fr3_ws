352c9cd5e27f9f83df04818a965ea40032774557
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-27 10:40:00 -0400

    New test value

--- src/gear_place/include/gear_place/motion_generators.hpp
+++ src/gear_place/include/gear_place/motion_generators.hpp
@@ -395,7 +395,7 @@ franka::Torques ForceMotionGenerator::operator()(const franka::RobotState &robot
   if ((counter_==125)){
     std::cout<<current_position[2]-initial_position_[2]<<std::endl;
   }
-  if ((abs(current_position[2]-initial_position_[2])<=0.000075 || (current_position[2]-initial_position_[2]>0.0)) && counter_>=125){
+  if ((abs(current_position[2]-initial_position_[2])<=0.000175 || (current_position[2]-initial_position_[2]>0.0)) && counter_>=125){
     std::cout <<"125"<<std::endl;
     return franka::MotionFinished(franka::Torques(tau_d_array));
   }
