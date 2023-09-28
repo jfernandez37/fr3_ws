a5fcf3d132fd9a294d0b629df72095027d2168e5
Joseph Fernandez <jothomas0615@gmail.com>
2023-09-28 16:28:27 -0400

    Testing

--- src/gear_place/nodes/gear_place_node.py
+++ src/gear_place/nodes/gear_place_node.py
@@ -12,7 +12,7 @@ def main(args=None):
         conveyor_supervisor = ConveyorClass("aprs_ros_conveyor")
         supervisor.wait(5)
         supervisor._call_open_gripper_service()
-        # supervisor._call_move_to_named_pose_service("home")
+        supervisor._call_move_to_named_pose_service("home")
         # supervisor._call_pick_up_multiple_gears(0.0095)
 
         supervisor._call_move_to_named_pose_service("above_conveyor")
