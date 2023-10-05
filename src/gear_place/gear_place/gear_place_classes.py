8f807f23df5b7405daa8a3181e4627ca2f1814cf
Joseph Fernandez <jothomas0615@gmail.com>
2023-10-05 14:30:56 -0400

    Does not try to pick up if second check fails

--- src/gear_place/gear_place/gear_place_classes.py
+++ src/gear_place/gear_place/gear_place_classes.py
@@ -706,7 +706,7 @@ class GearPlace(Node):
         self.get_logger().info(", ".join([str(val) for val in correct_gear]))
         if correct_gear.count(0.0)>=1 or correct_gear.count(None)>=1:
             self.get_logger().error("Second check above gear did not work. Attempting to pick up with current position")
-            self._call_pick_up_gear_coord_service(False,0.0,0.0, gear_point[2], object_width)
+            # self._call_pick_up_gear_coord_service(False,0.0,0.0, gear_point[2], object_width)
         else:
           self._call_pick_up_gear_coord_service(
               True, -1*correct_gear[1], -1*correct_gear[0],-1*correct_gear[2], object_width
